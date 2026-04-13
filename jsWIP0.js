4a4cac7254b53e420408465e50a7b89daf0f4a1a
Grayclouds815 <hgshreve@uncg.edu>
2026-04-13 19:58:50 -0400

    I changed the onclick function for the buttons in WIP0.html to call a new function called openNextButton, which takes the id of the next button as an argument. This way, when a button is clicked, it will call the openNextButton function and pass the id of the next button to be revealed. I also added an id to the first button for consistency.

--- jsWIP0.js
+++ jsWIP0.js
@@ -1,8 +1,19 @@
+
+let brightness= 255; 
+
 function openNextButton
 (nextButtonId) { 
-    let nextButtonElement = document.getElementById(nextButtonId);
+    let nextButtonElement = document.
+    getElementById(nextButtonId);
+    
     nextButtonElement.classList.
     remove('hidden');
+
+    let color= `rgb (${brightness}, ${brightness}, ${brightness})`;
+    nextButtonElement.style.
+    backgroundColor= color;
+
+    brightness -=50; 
 
 }
 
