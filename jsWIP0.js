ebe9d794bd4bebfe2680597936feecb22a7e966a
Grayclouds815 <hgshreve@uncg.edu>
2026-04-13 21:05:24 -0400

    I chnaged a lot, but mostly ficing the oclor changin aspect to make the first button change colors with allo the rest

--- jsWIP0.js
+++ jsWIP0.js
@@ -1,32 +1,43 @@
+let r = 180;
+let g = 160;
+let b = 255;
 
-let brightness = 255;
+let firstButton = document.getElementById('button1');
 
-let firsetButton = document.
-getElementById('button1');
+firstButton.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
-firstButton.style.backgroundColor = 
-`rgb(${brightness}, ${brightness}, 
-${brightness})`;
-;
-
-brightness -= 50; // 
-
-function openNextButton
-(nextButtonId) {
+function openNextButton(nextButtonId) {
   let nextButtonElement = document.getElementById(nextButtonId);
-
-
   nextButtonElement.classList.remove('hidden');
 
+  let color = `rgb(${r}, ${g}, ${b})`;
 
-  let color = `rgb(${brightness}, ${brightness}, ${brightness})`;
+  nextButtonElement.style.backgroundColor = color;
 
-  nextButtonElement.style.
-  backgroundColor = color;
+  if (textArea5) {
+    textArea5.style.backgroundColor = color;
+  }
 
-  textArea5.style.backgroundColor = color;
+  if (firstButton) {
+    firstButton.style.backgroundColor = color;
+    }       
 
 
-  brightness -= 50;
+  let fade = setInterval(() => {
+    if (r > 60 || g > 40 || b > 120) {
+      r -= 8;
+      g -= 2;
+      b -= 3;
+//-- if i change the numbers here it changes the speed of color change
+      let newColor = `rgb(${r}, ${g}, ${b})`;
 
+      nextButtonElement.style.backgroundColor = newColor;
+
+      if (textArea5) {
+        textArea5.style.backgroundColor = newColor;
+      }
+    } else {
+      clearInterval(fade);
+    }
+  }, 90);
 }
