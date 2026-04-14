c5695cfbfc865a6e3d9b15ba9e74618b33a35130
Grayclouds815 <hgshreve@uncg.edu>
2026-04-13 20:19:54 -0400

    I made edits to the WIP0.html file, adding onclick attributes to the buttons to call the openNextButton function, and I also added a script tag to link to the jsWIP0.js file. Additionally, I added an id attribute to the first button for better targeting in the JavaScript code.

--- jsWIP0.js
+++ jsWIP0.js
@@ -1,19 +1,32 @@
 
-let brightness= 255; 
+let brightness = 255;
+
+let firsetButton = document.
+getElementById('button1');
+
+firstButton.style.backgroundColor = 
+`rgb(${brightness}, ${brightness}, 
+${brightness})`;
+;
+
+brightness -= 50; // 
 
 function openNextButton
-(nextButtonId) { 
-    let nextButtonElement = document.
-    getElementById(nextButtonId);
-    
-    nextButtonElement.classList.
-    remove('hidden');
+(nextButtonId) {
+  let nextButtonElement = document.getElementById(nextButtonId);
 
-    let color= `rgb (${brightness}, ${brightness}, ${brightness})`;
-    nextButtonElement.style.
-    backgroundColor= color;
 
-    brightness -=50; 
+  nextButtonElement.classList.remove('hidden');
+
+
+  let color = `rgb(${brightness}, ${brightness}, ${brightness})`;
+
+  nextButtonElement.style.
+  backgroundColor = color;
+
+  textArea5.style.backgroundColor = color;
+
+
+  brightness -= 50;
 
 }
-
